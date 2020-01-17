10c2535693161bdbe77dbf2e1516c350e8c7fb39
Jason M <jbird206@gmail.com>
2020-01-17 10:01:37 -0800

    Add files via upload
    add initial back end logic

--- Programming Language Suggester/js/scripts.js
+++ Programming Language Suggester/js/scripts.js
@@ -1,1 +1,11 @@
-script crips
+$(document).ready(function() {
+  $("form#calculator").submit(function() {
+    event.preventDefault();
+    var number1 = parseInt($("#input1").val());
+    var number2 = parseInt($("#input2").val());
+    var number3 = parseInt($("#input3").val());
+    var number4 = parseInt($("#input4").val());
+    var number5 = parseInt($("#input5").val());
+
+    var result + number1 + number2 + number3 + number4 + number5;
+  }
