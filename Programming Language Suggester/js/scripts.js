d21e5addf0374483dbb5fb3e0a374bb9c1dc1753
Jason M <jbird206@gmail.com>
2020-01-17 11:19:20 -0800

    Add files via upload
    push from experimental branch. front end logic, and a CSS syyles element.

--- Programming Language Suggester/js/scripts.js
+++ Programming Language Suggester/js/scripts.js
@@ -1,1 +1,19 @@
-script crips
+$(document).ready(function() {
+  $("form#calculator").submit(function() {
+    event.preventDefault();
+    var number1 = parseInt($("#input1").val());
+    var number2 = parseInt($("#input2").val());
+    var number3 = parseInt($("#input3").val());
+    var number4 = parseInt($("#input4").val());
+    var number5 = parseInt($("#input5").val());
+
+    var result = number1 + number2 + number3 + number4 + number5;
+
+
+  $("#outpt").text(result);
+  if (result < 10) {
+    $("#python").show();
+    $("form#calculator").hide()
+ }
+});
+});
